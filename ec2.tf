91b6344681296ac868eba29802d0b1e00cfb8f37
MrBaykal <ebaykal2266@gmail.com>
2024-03-24 15:42:19 +0100

    EC2.tf and output.tf added

---
+++ ec2.tf
@@ -0,0 +1,29 @@
+# Private EC2
+
+resource "aws_instance" "private" {
+    ami = data.aws_ami.amazon_linux.id
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.privat.id
+    availability_zone = "${data.aws_region.current.name}a"
+    vpc_security_group_ids = [ aws_security_group.ssh.id]
+    key_name = "first_key"
+    tags = merge(
+        local.comman_tags,
+        tomap({ "Name" = "${local.prefix}-privat_ec2" })
+    )
+  
+}
+
+# Public EC2
+
+resource "aws_instance" "public" {
+    ami = data.aws_ami.amazon_linux.id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [ aws_security_group.ssh.id]
+    key_name = "first_key"
+    availability_zone = "${data.aws_region.current.name}a"
+    tags = merge(
+        local.comman_tags,
+        tomap({ "Name" = "${local.prefix}-public_ec2" })
+    )
+}
