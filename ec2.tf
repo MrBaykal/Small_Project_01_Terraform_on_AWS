d5a4d03cf051eae507ca69285e0e6a64cda74224
MrBaykal <ebaykal2266@gmail.com>
2024-03-26 14:19:09 +0100

    Files are updated

--- ec2.tf
+++ ec2.tf
@@ -3,13 +3,13 @@
 resource "aws_instance" "private" {
     ami = data.aws_ami.amazon_linux.id
     instance_type = var.instance_type
-    subnet_id = aws_subnet.privat.id
+    subnet_id = aws_subnet.private.id
     availability_zone = "${data.aws_region.current.name}a"
-    vpc_security_group_ids = [ aws_security_group.ssh.id]
+    vpc_security_group_ids = [ aws_security_group.ssh.id, ]
     key_name = "first_key"
     tags = merge(
         local.comman_tags,
-        tomap({ "Name" = "${local.prefix}-privat_ec2" })
+        tomap({ "Name" = "${local.prefix}-private_ec2" })
     )
   
 }
@@ -19,6 +19,7 @@ resource "aws_instance" "private" {
 resource "aws_instance" "public" {
     ami = data.aws_ami.amazon_linux.id
     instance_type = var.instance_type
+    subnet_id = aws_subnet.public.id
     vpc_security_group_ids = [ aws_security_group.ssh.id]
     key_name = "first_key"
     availability_zone = "${data.aws_region.current.name}a"
--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 # output
 output "ec2_public_ip" {
     description = "public ip of ec2"
-    value = aws_instance.public_ip
+    value = aws_instance.public
   
 }
